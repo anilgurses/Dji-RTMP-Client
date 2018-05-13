05b62292b2a700204683c41afcaa4465670a79b7
anlgrses <30665608+anlgrses@users.noreply.github.com>
2018-05-13 23:43:36 +0300

    Video preview bug fixed

--- SDK Swift Sample/ViewController.swift
+++ SDK Swift Sample/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import DJISDK
 import VideoPreviewer
 import AVFoundation
+import HaishinKit
 
 class ViewController: UIViewController, DJIVideoFeedListener, DJISDKManagerDelegate, DJICameraDelegate{
     
@@ -26,6 +27,7 @@ class ViewController: UIViewController, DJIVideoFeedListener, DJISDKManagerDeleg
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        DJISDKManager.registerApp(with: self)
         
         do {
             try session.setPreferredSampleRate(44_100)
@@ -33,6 +35,7 @@ class ViewController: UIViewController, DJIVideoFeedListener, DJISDKManagerDeleg
             try session.setMode(AVAudioSessionModeDefault)
             try session.setActive(true)
         } catch {
+            statusBar.text = "Error while connecting to server"
         }        // Do any additional setup after loading the view.
     }
     
@@ -46,9 +49,11 @@ class ViewController: UIViewController, DJIVideoFeedListener, DJISDKManagerDeleg
     }
 
     
-    func videoFeed(_ videoFeed: DJIVideoFeed, didUpdateVideoData videoData: Data) {
-        VideoPreviewer.instance().push(UInt8(videoData.bytes), length: Int(videoData.length))
-        //push(UInt8(videoData.bytes), length: Int(videoData.length))
+    func videoFeed(_ videoFeed: DJIVideoFeed, didUpdateVideoData rawData: Data) {
+        let videoData = rawData as NSData
+        let videoBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: videoData.length)
+        videoData.getBytes(videoBuffer, length: videoData.length)
+        VideoPreviewer.instance().push(videoBuffer, length: Int32(videoData.length))
     }
     
     func appRegisteredWithError(_ error: Error?) {
@@ -56,52 +61,93 @@ class ViewController: UIViewController, DJIVideoFeedListener, DJISDKManagerDeleg
     }
     
     func setupVideoPreviewer() {
-        VideoPreviewer.instance().setView(CameraView)
-        let product: DJIBaseProduct? = DJISDKManager.product()
-        if product?.model == DJIAircraftModelNameA3 || product?.model == DJIAircraftModelNameN3 || product?.model == DJIAircraftModelNameMatrice600 || product?.model == DJIAircraftModelNameMatrice600Pro {
+        
+        VideoPreviewer.instance().setView(self.CameraView)
+        let product = DJISDKManager.product();
+        
+        //Use "SecondaryVideoFeed" if the DJI Product is A3, N3, Matrice 600, or Matrice 600 Pro, otherwise, use "primaryVideoFeed".
+        if ((product?.model == DJIAircraftModelNameA3)
+            || (product?.model == DJIAircraftModelNameN3)
+            || (product?.model == DJIAircraftModelNameMatrice600)
+            || (product?.model == DJIAircraftModelNameMatrice600Pro)){
             DJISDKManager.videoFeeder()?.secondaryVideoFeed.add(self, with: nil)
-        } else {
+        }else{
             DJISDKManager.videoFeeder()?.primaryVideoFeed.add(self, with: nil)
         }
         VideoPreviewer.instance().start()
     }
-    
     func resetVideoPreview() {
         VideoPreviewer.instance().unSetView()
-        let product: DJIBaseProduct? = DJISDKManager.product()
-        if product?.model == DJIAircraftModelNameA3 || product?.model == DJIAircraftModelNameN3 || product?.model == DJIAircraftModelNameMatrice600 || product?.model == DJIAircraftModelNameMatrice600Pro {
+        let product = DJISDKManager.product();
+        
+        //Use "SecondaryVideoFeed" if the DJI Product is A3, N3, Matrice 600, or Matrice 600 Pro, otherwise, use "primaryVideoFeed".
+        if ((product?.model == DJIAircraftModelNameA3)
+            || (product?.model == DJIAircraftModelNameN3)
+            || (product?.model == DJIAircraftModelNameMatrice600)
+            || (product?.model == DJIAircraftModelNameMatrice600Pro)){
             DJISDKManager.videoFeeder()?.secondaryVideoFeed.remove(self)
-        } else {
+        }else{
             DJISDKManager.videoFeeder()?.primaryVideoFeed.remove(self)
         }
     }
     
     func fetchCamera() -> DJICamera? {
-        if !(DJISDKManager.product() != nil) {
+        let product = DJISDKManager.product()
+        
+        if (product == nil) {
             return nil
         }
-        if (DJISDKManager.product() is DJIAircraft) {
-            return (DJISDKManager.product() as? DJIAircraft)?.camera
-        } else if (DJISDKManager.product() is DJIHandheld) {
-            return (DJISDKManager.product() as? DJIHandheld)?.camera
+        
+        if (product!.isKind(of: DJIAircraft.self)) {
+            return (product as! DJIAircraft).camera
+        } else if (product!.isKind(of: DJIHandheld.self)) {
+            return (product as! DJIHandheld).camera
         }
         return nil
     }
     
     func productConnected(_ product: DJIBaseProduct?) {
-        if product != nil {
-            product?.delegate = self as? DJIBaseProductDelegate
-            let camera: DJICamera? = fetchCamera()
-            if camera != nil {
-                camera?.delegate = self
+        
+        NSLog("Product Connected")
+        
+        if (product != nil) {
+            let camera = self.fetchCamera()
+            if (camera != nil) {
+                camera!.delegate = self
             }
-            setupVideoPreviewer()
+            self.setupVideoPreviewer()
         }
+        
+
+        DJISDKManager.userAccountManager().logIntoDJIUserAccount(withAuthorizationRequired: false) { (state, error) in
+            if(error != nil){
+                NSLog("Login failed: %@" + String(describing: error))
+            }
+        }
+        
     }
     
+    func productDisconnected() {
+        
+        NSLog("Product Disconnected")
+        
+        let camera = self.fetchCamera()
+        if((camera != nil) && (camera?.delegate?.isEqual(self))!){
+            camera?.delegate = nil
+        }
+        self.resetVideoPreview()
+    }
 
     @IBAction func connectBtn(_ sender: Any) {
-        
+        let strurl = url.text
+        let rtmpConnection:RTMPConnection = RTMPConnection()
+        let rtmpStream: RTMPStream = RTMPStream(connection: rtmpConnection)
+        rtmpStream.attachScreen(ScreenCaptureSession(shared: UIApplication.shared))
+        let lfView: LFView = LFView(frame: view.bounds)
+        lfView.attachStream(rtmpStream)
+        rtmpConnection.connect(strurl!)
+        rtmpStream.publish("deneme")
+        statusBar.text = "Connection is started"
     }
     
     
